b1663d070531cf100902a9b41e9d6e6de2dfe44b
Aditya Kumar <97581083+adityakumartwo@users.noreply.github.com>
2026-01-14 23:17:24 +0530

    final commit

--- main.c
+++ main.c
@@ -12,16 +12,20 @@
 int main(){
 
     struct Student students[MAX_STUDENTS];
+
     int count = 0;
 
     printf("--- Student Result System __\n");
+
     printf("Reading data from input.txt ...\n");
 
     // I. Read Input, which take the input from the file.
     count = read_Input(students, MAX_STUDENTS);
 
     if(count == 0){
+
         printf("No valid records found.\n");
+        
         return 0;
     }
 
--- stu_header.h
+++ stu_header.h
@@ -5,23 +5,28 @@
 
 // Constants
 #define MAX_STUDENTS 100
+
 #define SUBJECTS 5
 
 // Structures
-struct Subject
-{
+struct Subject{
     float minor;
+
     float major;
+
     float total;
 };
 
-struct Student
-{
+struct Student {
     char id[100];
+
     char name[60];
+
     struct Subject subjects[SUBJECTS];
+
     float grandTotal;
     float percentage;
+
     char grade[3];
     float cgpa;
     int isFail;
@@ -29,13 +34,20 @@ struct Student
 
 // Function Prototypes like outline functions
 int read_Input(struct Student s[], int max_size);
+
 void calcu_Results(struct Student s[], int count);
+
 void show_Output(struct Student s[], int count);
+
+
 
 // Functions that are for the internal use
 int valid_ID(char *id, struct Student s[], int count);
+
 int valid_Name(char *name);
+
 int valid_Marks(float minor, float major);
+
 void show_Grade(float p, int fail, char *gradeStr, float *cgpa);
 
 #endif
